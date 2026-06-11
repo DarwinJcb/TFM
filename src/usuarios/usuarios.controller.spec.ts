889c36b99e8998f2056663c452dc94a8ae14f14a
Dark <djcabrera@sudamericano.edu.ec>
2026-06-11 13:33:27 -0500

    Commit #07

---
+++ src/usuarios/usuarios.controller.spec.ts
@@ -0,0 +1,21 @@
+/* src/usuarios/usuarios.controller.spec.ts: */
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsuariosController } from './usuarios.controller';
+import { UsuariosService } from './usuarios.service';
+
+describe('UsuariosController', () => {
+  let controller: UsuariosController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsuariosController],
+      providers: [UsuariosService],
+    }).compile();
+
+    controller = module.get<UsuariosController>(UsuariosController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
