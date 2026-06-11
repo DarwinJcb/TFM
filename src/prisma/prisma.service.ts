b42c9859de38def01cb0bfb912b00cc2fbdd3598
Dark <djcabrera@sudamericano.edu.ec>
2026-06-11 13:02:08 -0500

    Commit #04

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,16 @@
 /* src/prisma/prisma.service.ts: */
+import 'dotenv/config';
 import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client.js';
 
 @Injectable()
-export class PrismaService { }
+export class PrismaService extends PrismaClient {
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL as string,
+        });
+
+        super({ adapter });
+    }
+}
